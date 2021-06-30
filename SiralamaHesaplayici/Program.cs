cd3572efe43ee81f1fd630d8c7dc81e4b48a40bb
RuzgarSepci <73252123+RuzgarSepci@users.noreply.github.com>
2021-06-30 23:41:07 +0300

    Add files via upload

---
+++ SiralamaHesaplayici/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SiralamaHesaplayici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yüzdelik diliminiz (yüzde):");
+            decimal dilim = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("\nSınava giren kişi sayısı:");
+            ulong kisi = Convert.ToUInt64(Console.ReadLine());
+            decimal siralama = dilim * kisi / 100;
+            Console.WriteLine("\nSıralamanız: " + Convert.ToString(siralama));
+            Console.ReadKey();
+        }
+    }
+}
